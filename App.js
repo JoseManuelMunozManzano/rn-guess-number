7aa4d6ce61095c183e1b754b7f4af9d80abb250c
Neimerc <Neil_Mercury@yahoo.es>
2021-03-20 21:23:21 +0100

    Adding Custom Fonts

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { Header } from './components/Header';
 import { GameOverScreen } from './screens/GameOverScreen';
 import { GameScreen } from './screens/GameScreen';
 import { StartGameScreen } from './screens/StartGameScreen';
+import { fetchFonts } from './helper/fetchFonts';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await fetchFonts();
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setDataLoaded(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (dataLoaded) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [dataLoaded]);
+
+  if (!dataLoaded) {
+    return null;
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -39,7 +78,7 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <View style={styles.screen} onLayout={onLayoutRootView}>
       <Header title="Guess a Number" />
       {content}
     </View>
